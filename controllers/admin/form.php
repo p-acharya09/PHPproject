7e2ff9bb6ea56eb8ff8f0a799774e71aff157c64
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-11-27 13:47:46 -0800

    Upload images

--- controllers/admin/form.php
+++ controllers/admin/form.php
@@ -4,12 +4,17 @@ $db = new Database($config['database']);
 
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    
+    $uploadDir = 'uploads/';
+    $fileTmp = $_FILES['image']['tmp_name'];
+    $fileName = basename($_FILES['image']['name']); 
+    $fileDestination = $uploadDir . $fileName; // Destination path
+    move_uploaded_file($fileTmp, $fileDestination); 
     $title = $_POST['title'];
     $subtitle = $_POST['subtitle'];
-    $result = $db->query('INSERT INTO `media` (`title`, `subtitle`) VALUES (:title, :subtitle)',[
+    $result = $db->query('INSERT INTO `media` (`title`, `subtitle`,`image`) VALUES (:title, :subtitle, :images)',[
         'title' => $title,
-        'subtitle' => $subtitle
+        'subtitle' => $subtitle,
+        'images' => $fileDestination,
     ]);
     require "views/admin/form.view.php";
     }else{
--- views/admin/form.view.php
+++ views/admin/form.view.php
@@ -18,7 +18,7 @@
                             <i class="fas fa-list mr-3"></i> Media Form
                         </p>
                         <div class="leading-loose">
-                            <form action="" method="POST" class="p-10 bg-white rounded shadow-xl">
+                            <form action="" method="POST" class="p-10 bg-white rounded shadow-xl" enctype="multipart/form-data">
                                 <div class="">
                                     <label class="block text-sm text-gray-600" for="title">Title</label>
                                     <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="title" name="title" type="text" required="" placeholder="Title" aria-label="Name">
@@ -27,9 +27,9 @@
                                     <label class="block text-sm text-gray-600" for="subtitle">Sub Title</label>
                                     <input class="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="subtitle" name="subtitle" type="text" required="" placeholder="Subtitle" aria-label="Subtitle">
                                 </div>
-                                <div class="" hidden>
+                                <div class="mt-2">
                                     <label class="block text-sm text-gray-600" for="image">Image</label>
-                                    <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="image" name="image" type="text" required="" placeholder="Image" aria-label="Image" value="samrat">
+                                    <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="image" name="image" type="file" required="" placeholder="Image" aria-label="Image" value="">
                                 </div>
                                 <div class="mt-6">
                                     <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">Submit</button>
