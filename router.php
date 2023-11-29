55149d11e3af21d372632277a9d6f904afb6c651
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-11-28 16:20:55 -0800

    Add project root to the code

--- router.php
+++ router.php
@@ -2,17 +2,20 @@
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
+$proj_root = '/';
+// incase of subfolders $proj_root = '/folder_name/'
+
 $routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-    '/media' => 'controllers/media.php',
-    '/artdetail' => 'controllers/artdetail.php',
-    '/login' =>'controllers/login/create.php',
-    '/adminforms' => 'controllers/admin/form.php',
-    '/admintables' => 'controllers/admin/list.php',
-    '/adminabout' => 'controllers/admin/about.php',
-    '/adminsettings' => 'controllers/admin/settings.php'
+    $proj_root.'/' => 'controllers/index.php',
+    $proj_root.'about' => 'controllers/about.php',
+    $proj_root.'contact' => 'controllers/contact.php',
+    $proj_root.'media' => 'controllers/media.php',
+    $proj_root.'artdetail' => 'controllers/artdetail.php',
+    $proj_root.'login' => 'controllers/login/create.php',
+    $proj_root.'adminforms' => 'controllers/admin/form.php',
+    $proj_root.'admintables' => 'controllers/admin/list.php',
+    $proj_root.'adminabout' => 'controllers/admin/about.php',
+    $proj_root.'adminsettings' => 'controllers/admin/settings.php'
 ];
 
 function routeToController($uri, $routes) {
@@ -26,7 +29,8 @@ function routeToController($uri, $routes) {
 function abort($code = 404) {
     http_response_code($code);
 
-    require "views/{$code}.php";
+    // require "views/{$code}.php";
+    echo ("Failed. 404 error");
 
     die();
 }
