048739ac2fde5556fb1537c75cebf0f540b653c6
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-11-28 21:13:26 -0800

    Dynamic art detail page

--- controllers/artdetail.php
+++ controllers/artdetail.php
@@ -1,10 +1,20 @@
-<?php 
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$uri_segments = explode('/', rtrim($uri, '/'));
+$id = end($uri_segments);
+
+if (!ctype_digit($id)) {
+    require "views/404.php";
+    die();
+}
+
 $config = require('config.php');
 $db = new Database($config['database']);
 
 $contact = $db->query('select * from settings')->fetch();
-$list = $db->query('select * from media where id=1')->fetch();
+$list = $db->query('select * from media where id='.$id)->fetch();
 $listall = $db->query('select * from media')->fetchAll();
+$listall = array_slice($listall, 0, 8);
 
 require "views/artdetail.view.php"; 
 ?>
--- views/artdetail.view.php
+++ views/artdetail.view.php
@@ -6,41 +6,20 @@
                 <h3><?= $list['title']?></h3>
                 <p><?= $list['subtitle']?></p>
             </div>
-            <img title="<?= $list['title']?>" src="./images/nkb.jpeg">
+            <img title="<?= $list['title']?>" src= <?= $list['images']?> >
         </section>
 
         <section class="suggested">
             <h2>Recommended ArtWorks</h2>
             <div class="suggested-art-detail">
-                <figure>
-                    <a href="./artdetail">
-                        <img title="Nabin K Bhattarai" src="./images/nkb.jpeg">
-                        <figcaption>Nabin K Bhattarai</figcaption>
-                        <p>Popular Nepali Singer</p>
-                    </a>
-                </figure>
-                <figure>
-                    <a href="./artdetail">
-                        <img title="Old Man" src="./images/ol.jpeg">
-                        <figcaption>Man</figcaption>
-                        <p>Lorem Ipsum </p>
-                    </a>
-                </figure>
-                <figure>
-                    <a href="./artdetail">
-                        <img title="Horse" src="./images/horse.jpeg">
-                        <figcaption>Horse</figcaption>
-                        <p>Lorem Ipsum </p>
-                    </a>
-                </figure>
-                <?php foreach ($listall as $data): ?>
-                <figure>
-                    <a href="./artdetail">
-                        <img title="<?= $data['title']?>" src="./images/mbtw.jpeg">
-                        <figcaption><?= $data['title']?></figcaption>
-                        <p><?= $data['subtitle']?></p>
-                    </a>
-                </figure>
+                <?php foreach($listall as $data):?>
+                    <figure>
+                        <a href="./artdetail/<?=$data['id']?>">
+                            <img title="<?= $data['title'] ?>" src="<?= $data['images']?>">
+                            <figcaption><?= $data['title'] ?></figcaption>
+                            <p><?= $data['subtitle'] ?></p>
+                        </a>
+                    </figure>
                 <?php endforeach ?>
             </div>
         </section>
